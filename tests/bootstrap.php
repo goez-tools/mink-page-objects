ad0256d890591c856087cc9b07a0d36b7bb19610
Jace Ju <jaceju@gmail.com>
2015-10-30 14:43:59 +0800

    Change path of unit test cases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,4 @@
 <?php
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->addPsr4('Example\\', __DIR__ . '/Example/');
+$loader->addPsr4('Example\\', __DIR__ . '/unit/Example/');
