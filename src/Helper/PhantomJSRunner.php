a92f3391319f20a9786d6cd50501979811675bb9
Jace Ju <jaceju@gmail.com>
2015-11-03 10:42:40 +0800

    Change strategy of starting phantomjs

--- src/Helper/PhantomJSRunner.php
+++ src/Helper/PhantomJSRunner.php
@@ -9,38 +9,9 @@ trait PhantomJSRunner
      */
     protected function startPhantomJS()
     {
-        if ($this->ifPhantomJsAlreadyRunning()) {
-            $this->printMessage("-");
-        } else {
-            $this->printMessage("|");
-            $this->runPhantomJS();
-        }
-    }
-
-    /**
-     * @return resource
-     */
-    protected function ifPhantomJsAlreadyRunning()
-    {
-        set_error_handler(function () {});
-        $result = fsockopen("localhost", 4444);
-        fclose($result);
-        restore_error_handler();
-        return !!$result;
-    }
-
-    protected function runPhantomJS()
-    {
+        shell_exec("pkill phantomjs");
         $cmd = 'phantomjs --webdriver=4444 --ssl-protocol=tlsv1 --ignore-ssl-errors=true';
         shell_exec($cmd . " > /dev/null &");
         sleep(1);
-    }
-
-    /**
-     * @param $message
-     */
-    protected function printMessage($message)
-    {
-        fwrite(STDOUT, "$message");
     }
 }
