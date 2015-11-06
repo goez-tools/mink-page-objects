f8b5a512f789b96cb167c52e431ca5ea206736e4
Jace Ju <jaceju@gmail.com>
2015-11-06 14:50:17 +0800

    Replace @before with @beforeClass to start phantomjs

--- src/Helper/PhantomJSRunner.php
+++ src/Helper/PhantomJSRunner.php
@@ -5,13 +5,21 @@ namespace Goez\PageObjects\Helper;
 trait PhantomJSRunner
 {
     /**
-     * @before
+     * @beforeClass
      */
-    protected function startPhantomJS()
+    public static function startPhantomJS()
     {
         shell_exec("pkill phantomjs");
         $cmd = 'phantomjs --webdriver=4444 --ssl-protocol=tlsv1 --ignore-ssl-errors=true';
         shell_exec($cmd . " > /dev/null &");
         sleep(1);
+    }
+
+    /**
+     * @afterClass
+     */
+    public static function stopPhantomJS()
+    {
+        shell_exec("pkill phantomjs");
     }
 }
